93233a34718a8ad8e2c57cf63e3fbbd1cc3f3ab5
Edwin Jimenez <ejjimenezc@unal.edu.co>
2019-11-28 20:20:35 -0500

    Merge sgs-develop into master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import './App.css';
-import './App.scss';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Inicio from './Inicio';
+import Informacion from './Informacion';
+import Resumen from './Resumen';
+import Resultado from './Resultado';
 
-import Questions from './components/Questions';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Safe House'
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Questions name="TPI" />
-    </div>
-  );
+  render() {
+
+    return (
+          <Router>
+
+          {/* Aqui se configuran las rutas de la aplicacion */}
+
+            {/* Ruta al Home de la aplicacion */}
+            <Route exact path = "/" render = { () => {
+                return (
+                  <Informacion/>
+                )
+              }
+              }>
+            </Route>
+
+            {/* Ruta a la pagina de informacion/preguntas */}
+            <Route exact path = "/informacion" render = { () => {
+                return (
+                  <Informacion/>
+                )
+              }
+              }>
+            </Route>
+
+            {/* Ruta a la pagina de los resultados */}
+            <Route exact path = "/resultado" render = {() => {
+              return (
+                <Resultado/>
+              )
+            }
+            }>
+            </Route>
+
+            {/* Ruta a la pagina de resumen */}
+            <Route exact path = "/resumen" render = {() => {
+              return (
+                <Resumen/>
+              )
+            }
+            }>
+            </Route>
+          </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Informacion.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Inf_basica from './Inf_basica';
+import Inf_tecno from './Inf_tecno';
+import Resumen from './Resumen';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    padding: theme.spacing(1),
+
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(4),
+    marginRight: theme.spacing(4),
+    [theme.breakpoints.up(600 + theme.spacing(4) * 4)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const steps = ['Información básica', 'Información tecnológica', 'Tu resumen'];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <Inf_basica/>;
+    case 1:
+      return <Inf_tecno/>;
+    case 2:
+      return <Resumen />;
+    default:
+      throw new Error('Paso desconocido');
+  }
+}
+
+export default function Checkout() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  return (
+    <div className={classes.image}>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Safe Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            Información de tu hogar
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map(label => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Gracias por confiar en nosotros
+                </Typography>
+                <Typography variant="subtitle1">
+                  Oprime el botón para avanzar a tu cotización sugerida
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Ver cotización
+                </Button>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <div className={classes.buttons}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} className={classes.button}>
+                      Anterior
+                    </Button>
+                  )}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Finalizar' : 'Siguiente'}
+                  </Button>
+                </div>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        </Paper>
+      </main>
+    </React.Fragment>
+    </div>
+  );
+}
